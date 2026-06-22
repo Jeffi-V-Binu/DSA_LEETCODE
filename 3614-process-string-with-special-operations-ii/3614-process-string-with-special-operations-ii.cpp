287f7d9bcae84da4440491ee0d2b4679c3ced13f
Jeffi V Binu <jeffibinu@gmail.com>
2026-06-22 22:34:51 +0530

    Time: 36 ms (29.47%), Space: 24 MB (60.31%) - LeetHub

--- 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
@@ -1,3 +1,8 @@
+static const int speedup = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
 class Solution {
 public:
     char processStr(string s, long long k) {
