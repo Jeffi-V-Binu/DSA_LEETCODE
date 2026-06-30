3303f733136d996c6c50a9e358d36c3a507b2a2a
Jeffi V Binu <jeffibinu@gmail.com>
2026-06-30 23:21:58 +0530

    Time: 1 ms (75.79%), Space: 165.8 MB (58.27%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int n = nums1.size();
-        if(n < 2) return true;
         int smallnum = 1000000000;
         int oddc  = 0;
-        int evenc = 0;
         for(const int& i: nums1){
             smallnum = min(smallnum , i);
-            if(i%2 == 0) ++evenc;
-            else ++oddc;
+            if(i%2 == 1) ++oddc;
         }
         if(smallnum % 2 == 0 && oddc) return false;
         return true;
