0ae3832c633b99e8ee6302f14ce61f7a69eed033
Jeffi V Binu <jeffibinu@gmail.com>
2026-07-01 21:29:06 +0530

    Time: 0 ms (100%), Space: 7.9 MB (88.64%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int total = m + n - 2;
-        int mind = min(m - 1 , n - 1);
+        short total = m + n - 2;
+        char mind = min(m - 1 , n - 1);
         long long result = 1;
         for(int i = 1 ; i <= mind ; ++i){
             result = result * (total - mind + i) / i ;
