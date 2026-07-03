7ae5c7aa76f1f60581cbb828d7e7928f126a6dff
Jeffi V Binu <jeffibinu@gmail.com>
2026-07-03 16:00:04 +0530

    Time: 9 ms (29.1%), Space: 20.1 MB (33.98%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        numj = 0
+        currw = 0
+        furtheste = 0
+        for i in range(len(nums) - 1):
+            furtheste = max(furtheste , i + nums[i])
+            if i == currw:
+                numj += 1
+                currw = furtheste
+            if currw >= len(nums) - 1:
+                break
+        return numj
+
+
+        
