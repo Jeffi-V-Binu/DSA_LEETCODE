a5ff301dbf0787286ab6f9a692dbe9442130d8b9
Jeffi V Binu <jeffibinu@gmail.com>
2026-07-03 11:34:30 +0530

    Time: 986 ms (50.06%), Space: 68.4 MB (52.78%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -17,18 +17,19 @@ class WordDictionary:
 
     def search(self, word: str) -> bool:
         def dfs(index , node):
-            if index == len(word):
-                return node.word_end
-            ch = word[index]
-            if ch == ".":
-                for child_node in node.children.values():
-                    if dfs(index + 1, child_node):
-                        return True
-                return False
-            else:
-                if ch in node.children:
-                    return dfs(index + 1, node.children[ch])
-                return False
+            current = node
+            for i in range(index , len(word)):
+                ch = word[i]
+                if ch == ".":
+                    for child in current.children.values():
+                        if dfs(i + 1 , child):
+                            return True
+                    return False
+                else:
+                    if ch not in current.children:
+                        return False
+                    current = current.children[ch]
+            return current.word_end
         return dfs(0 , self.root)
         
 
