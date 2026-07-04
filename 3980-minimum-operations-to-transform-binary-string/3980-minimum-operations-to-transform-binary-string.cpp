5c669aafeaef30b44cdc8423dfde35698cd5e12e
Jeffi V Binu <jeffibinu@gmail.com>
2026-07-04 23:20:42 +0530

    Time: 12 ms (73.41%), Space: 25.5 MB (99.89%) - LeetHub

--- 3980-minimum-operations-to-transform-binary-string/3980-minimum-operations-to-transform-binary-string.cpp
+++ 3980-minimum-operations-to-transform-binary-string/3980-minimum-operations-to-transform-binary-string.cpp
@@ -4,29 +4,24 @@ public:
         int count = 0;
         int n = s1.size();
         if(n == 1 && s1[0] == '1' && s2[0] == '0') return -1;
-        for(int i = 0 ; i < n - 1 ; ++i){
+        for(int i = 0 ; i < n  ; ++i){
             if(s1[i] != s2[i]){
                 char a = s1[i];
                 char b = s2[i];
                 if(a == '0'){
                     ++count;}
                 else{
-                    if(s1[i + 1] == '1'){
-                        ++count;
-                        s1[i+1] = '0';
+                    if(i < n - 1 && s2[i + 1] == '0'){
+                        if(s1[i + 1] == '1'){
+                            ++count;
+                        }
+                        else{
+                            count +=2;
+                        }
+                        ++i;
                     }
                     else{
                         count += 2;}}}}
-        char c = s1[n - 1];
-        char d = s2[n - 1];
-        if(c != d){
-            if(c == '0'){
-                ++count;
-            }
-            else{
-                count += 2;
-            }
-        }
         return count;
     }
 };
