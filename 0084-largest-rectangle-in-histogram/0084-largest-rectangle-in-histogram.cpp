b8332e7bd6342e590e31e2afadec2a42584bbce9
Jeffi V Binu <jeffibinu@gmail.com>
2026-07-06 23:28:10 +0530

    Time: 6 ms (98.56%), Space: 78.7 MB (99.4%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -8,15 +8,15 @@ public:
         int n = heights.size();
         for(int i = 0 ; i < n ; ++i){
             while(top != -1 && heights[st[top]] > heights[i]){
-                if(top > 0 && heights[st[top]] == heights[st[top - 1]]){
-                    --top;
-                    st.pop_back();
-                }
                 if(top == 0) maxh = max(maxh , (i * heights[st[top]]));
                 else maxh = max(maxh , ((i - st[top - 1] - 1) * heights[st[top]]));
                 --top;
                 st.pop_back();
             }
+            if(top != -1 && heights[st[top]] == heights[i]){
+                --top;
+                st.pop_back();
+            };
             ++top;
             st.push_back(i);
         }
