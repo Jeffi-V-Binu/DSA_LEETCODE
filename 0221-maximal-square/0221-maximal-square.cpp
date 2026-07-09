2c8568d3ec1b07f3379ff163800273b9f240825b
Jeffi V Binu <jeffibinu@gmail.com>
2026-07-09 23:18:30 +0530

    Time: 4 ms (87.41%), Space: 27.7 MB (90.37%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -3,20 +3,32 @@ public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>> map(n , vector<int>(m , 0));
+        vector<int>map(m , 0);
         int result = 0;
+        int temp = 0;
         for(int i = 0; i < n ; ++i){
             for(int j = 0 ; j < m ; ++j){
                 if(i == 0 || j == 0){
                     if(matrix[i][j] == '1'){
                         result = max(result , 1);
-                        map[i][j] = 1;
+                        temp = map[j];
+                        map[j] = 1;
+                    }
+                    else{
+                        temp = map[j];
+                        map[j] = 0;
                     }
                 }
                 else{
                     if(matrix[i][j] == '1'){
-                        map[i][j] = min(map[i][j - 1] , min( map[ i - 1][j] , map[i - 1][ j - 1])) + 1;
-                        result = max(result , map[i][j]);
+                        int t = map[j];
+                        map[j] = min(map[j - 1] , min( map[j] , temp)) + 1;
+                        temp = t;
+                        result = max(result , map[j]);
+                    }
+                    else{
+                        temp = map[j];
+                        map[j] = 0;
                     }
                 }
             }
