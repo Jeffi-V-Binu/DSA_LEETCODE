b7f20daafe5d76921ba63359a4f5d111e29f5edd
Jeffi V Binu <jeffibinu@gmail.com>
2026-07-09 22:45:56 +0530

    Time: 0 ms (100%), Space: 224.1 MB (66.03%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
         vector<bool> result;
+        int m = queries.size();
+        result.reserve(m);
         vector<int> partitions(n , 1);
         int count = 1;
         for(int i = 1 ; i < n ; ++i){
             if(nums[i - 1] + maxDiff < nums[i]) ++count;
             partitions[i] = count;
         }
-        for(const auto& a: queries){
-            if(partitions[a[0]] == partitions[a[1]]) result.push_back(true);
+        for(int i = 0 ; i < m ; ++i){
+            if(partitions[queries[i][0]] == partitions[queries[i][1]]) result.push_back(true);
             else result.push_back(false);
         }
         return result;
