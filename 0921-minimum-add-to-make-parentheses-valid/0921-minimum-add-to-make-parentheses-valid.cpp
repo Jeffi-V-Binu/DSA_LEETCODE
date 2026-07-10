6f6404f75599bd3cb5c3958c0422449f50d61bcf
Jeffi V Binu <jeffibinu@gmail.com>
2026-07-10 21:49:38 +0530

    Time: 0 ms (100%), Space: 8.5 MB (56.18%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int l = 0;
+        int r = 0;
+        for(const char a: s){
+            if(a == '(') ++l;
+            else {
+                if(l == 0) r++;
+                else --l;
+            }
+        }
+        return (l + r);
+    }
+};
