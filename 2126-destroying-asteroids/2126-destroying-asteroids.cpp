e7ac37fb51388be5a02f7aad8e286e3573d1f11c
Jeffi V Binu <jeffibinu@gmail.com>
2026-07-10 18:36:27 +0530

    Time: 35 ms (38.19%), Space: 106.8 MB (16.31%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        long long tm = mass;
+        sort(asteroids.begin() , asteroids.end());
+        for(const int i : asteroids){
+            if(i > tm) return false;
+            tm += i;
+        }
+        return true;
+    }
+};
