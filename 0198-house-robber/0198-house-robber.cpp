df28383f03e2ecf86e2189bb2d25108c61b16d1b
Jeffi V Binu <jeffibinu@gmail.com>
2026-07-14 22:23:17 +0530

    Time: 0 ms (100%), Space: 10.1 MB (89.82%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,14 +4,14 @@ public:
         int n = nums.size();
         if(n == 1) return nums[0];
         else if(n == 2) return max(nums[1] , nums[0]);
-        vector<int> dp(n , 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[1] , nums[0]);
+        int odd = nums[0];
+        int even = max(nums[1] , nums[0]);
         for(int i = 2 ; i < n ; ++i){
-            int l = dp[i - 1];
-            int r = nums[i] + dp[i - 2];
-            dp[i] = max(l , r);
+            int l = even;
+            int r = nums[i] + odd;
+            odd = even;
+            even = max(l , r);
         }
-        return dp[n - 1];
+        return even;
     }
 };
