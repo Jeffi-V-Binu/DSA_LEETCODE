30c8715e16cc111b8826f5f699ee368788c67f14
Jeffi V Binu <jeffibinu@gmail.com>
2026-08-24 00:52:58 +0530

    Time: 3 ms (66.67%), Space: 14 MB (33.57%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.size() - 1;
+        int ls = 0;
+        int rs = 0;
+        int lq = 0;
+        int rq = 0;
+        int j = n;
+        int i = 0;
+        while(i < j){
+            if(num[i] == '?') ++lq;
+            else{
+                int temp = num[i] - '0';
+                ls += temp;
+            }
+            ++i;
+            if(num[j] == '?') ++rq;
+            else{
+                int temp = num[j] - '0';
+                rs += temp;
+            }
+            --j;
+        }
+        if(lq == 0 && rq == 0){
+            if(rs == ls) return false;
+            else return true;
+        }
+        if(rs == ls){
+            if(rq == lq) return false;
+            else return true;
+        }
+        else{
+            if((ls > rs && lq >= rq) || (rs > ls && rq >= lq)) return true;
+            else{
+                int sumd = abs(rs - ls);
+                int qd = abs(lq - rq);
+                int bm = qd / 2;
+                int am = (qd+1) / 2;
+                if(sumd == (bm * 9) && bm == am) return false;
+                else return true;
+            }
+        }
+        return true;
+    }
+};
