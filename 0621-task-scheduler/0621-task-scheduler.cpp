7a25dfefa3915ffab42fde8a44f64d8c142c9a3d
Jeffi V Binu <jeffibinu@gmail.com>
2026-08-24 21:01:21 +0530

    Time: 4 ms (69.57%), Space: 43 MB (47.74%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -4,15 +4,27 @@ public:
         vector<int> fcount(26 , 0);
         int m = tasks.size();
         int minidle;
+        priority_queue<int> maxheap;
         for(char a: tasks){
             fcount[a - 'A']++;
         }
-        sort(fcount.begin(), fcount.end());
-        int maxv = fcount[25];
-        minidle = (maxv - 1) * n;
-        for(int i = 24 ; i >= 0 ; --i){
-            minidle -= min((maxv-1) , fcount[i]);
+        for(int i : fcount) if(i != 0) maxheap.push(i);
+        int time = 0;
+        queue<pair<int , int>> cd;
+        while(!maxheap.empty() || !cd.empty()){
+            ++time;
+
+            if(!maxheap.empty()){
+                int count = maxheap.top() - 1;
+                maxheap.pop();
+                if(count > 0) cd.push({count , time + n});
+            }
+
+            if(!cd.empty() && cd.front().second == time){
+                maxheap.push(cd.front().first);
+                cd.pop();
+            }
         }
-        return max(m , (m + minidle));
+        return time;
     }
 };
